e0689d7db056b96b5cfb0e85a8497b1032952474
Reshu Singh <reshu70173@gmail.com>
2024-04-22 19:00:42 +0530

    Question Number: 80

---
+++ Medium/Remove Duplicates from Sorted Array II .cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+       int i = 0;
+       for(auto e : nums) {
+           if(i == 0 || i == 1 || e != nums[i - 2]) {
+              
+                   nums[i++] = e;
+                   i++;
+             
+           }
+       }
+        return i;
+    }
+};
